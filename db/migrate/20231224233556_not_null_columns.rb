ee0a5ebdd669048163948e74a7cc955374b70b6c
ThiagoDuarteC <thiagodcorreia01@gmail.com>
2024-09-29 18:27:04 -0300

    create the real project

--- db/migrate/20231224233556_not_null_columns.rb
+++
@@ -1,7 +0,0 @@
-class NotNullColumns < ActiveRecord::Migration[5.2]
-  def change
-    change_column :statuses, :name, :string, null: false
-    change_column :tasks, :id_event, :bigint, null: false
-    change_column :tasks, :title, :string, null: false
-  end
-end
